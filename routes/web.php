bfab973115b9be35b7a359341c6e7382d5e245ca
Abdessamad Zgor <abdessamadzgor@gmail.com>
2024-03-05 12:50:03 +0100

    new php blog

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $t = "anas z";
+    return view('welcome', ['t'=>$t]);
+});
+
+Route::get('/test', [TestController::class, 'index']);
+
